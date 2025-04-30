24dc1e25e291500c94db7bc11af85e4ee80f040a
msftstinks <msftstinks@gmail.com>
2025-04-30 12:25:46 +0530

    Add backend/models/Quiz.js 1745996144345

---
+++ backend/models/Quiz.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const quizSchema = new mongoose.Schema({
+  course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
+  title: { type: String, required: true },
+  questions: [{
+    questionText: { type: String, required: true },
+    options: [{ type: String, required: true }],
+    correctAnswerIndex: { type: Number, required: true }
+  }],
+}, { timestamps: true });
+
+module.exports = mongoose.model('Quiz', quizSchema);
